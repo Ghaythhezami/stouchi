2e57e425a0f343eefd3b98180840f3a5a4aaa896
ghayth-khezami <ghaith.khezami@digidco.com>
2026-03-04 04:58:08 +0100

    feat(ghayth): added fix

--- app.config.js
+++ app.config.js
@@ -1,23 +1,31 @@
 export default {
-  name: 'Stouchi',
-  slug: 'stouchi',
-  version: '1.0.0',
-  orientation: 'portrait',
-  icon: './src/assets/stouchi.png',
-  userInterfaceStyle: 'light',
-  splash: {
-    image: './src/assets/stouchi.png',
-    resizeMode: 'contain',
-    backgroundColor: '#000000',
-  },
-  assetBundlePatterns: ['**/*'],
-  android: {
-    package: 'com.stouchi.app',
-    versionCode: 1,
-    adaptiveIcon: {
-      foregroundImage: './src/assets/stouchi.png',
+  expo: {
+    name: 'Stouchi',
+    slug: 'stouchi',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './src/assets/stouchi.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './src/assets/stouchi.png',
+      resizeMode: 'contain',
       backgroundColor: '#000000',
+    },
+    assetBundlePatterns: ['**/*'],
+    android: {
+      package: 'com.stouchi.app',
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: './src/assets/stouchi.png',
+        backgroundColor: '#000000',
+      },
+    },
+    extra: {
+      eas: {
+        projectId: '1d7d6f7b-5322-4a7e-9c64-50f0e4a49c81',
+      },
     },
   },
 };
+
 
